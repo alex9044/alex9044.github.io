94a59d52b964ec3f0a4b9b01f8fca7f29d4e1232
Alex Moscato <alexmoscato9044@gmail.com>
2022-01-25 20:59:05 -0300

    Meu segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements OnInit{
+
+  //Variavel para armazenar os todos salvos
+  todos: Todo[] = []
+
+  //Variavel que controla o formulario
+  form: FormGroup = new FormGroup({
+    //description é um campo do formulario
+    description: new FormControl('', [Validators.required, Validators.minLength(4)])
+  })
+
+  constructor(
+    //Injeção de serviço, inicializa a clase 
+    private service : TodoService 
+  ){}
+  
+  //Metodo chamado antes de iniciar o componente
+  ngOnInit(){
+      this.listTodos();
+  }
+  
+  listTodos(){
+    this.service.list().subscribe(todoList => {
+      this.todos = todoList;
+    })
+  }
+
+  done(todo: Todo){
+    this.service.markAsDone(todo.id).subscribe({
+      //next method: recebe a resposta do metodo markAsDone
+      next: (todoAtualizado) => {
+        todo.done = todoAtualizado.done
+        todo.doneDate = todoAtualizado.doneDate
+      }
+    })
+  }
+
+  submit(){
+    //console.log: Imprime o valor do campo description
+    //this.form.value: cria um objeto com todos os valores do formulario form 
+    console.log(this.form.value)
+    //Modelo Todo
+    //...this.form.value: distribui os valores do formulario segundo os campos do modelo 
+    const todo: Todo =  {...this.form.value}
+    //Faz a requisição
+    this.service
+    .save(todo)
+    .subscribe(//Metodo subcribe trata o retorno da requisição 
+      //savedTodo: Objeto que foi retornado da api 
+      savedTodo => {
+        //Adiciona todas as todos no array 
+        this.todos.push(savedTodo)
+        //Reseta o formulario
+        this.form.reset()
+      }
+    )
+  }
+
+  delete(todo: Todo){
+    this.service.delete(todo.id).subscribe({
+      next: (response) => this.listTodos()
+    })
+  }
 }
---
+++ src/app/todo.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+//Rxjs: 
+import { Observable } from 'rxjs';
+//HttpClient: Classe para usar serviços client HTTP
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+
+//Classe responsalve por fazer as requisições Http 
+export class TodoService {
+  
+  //Url da api 
+  apiUrl: string = 'http://localhost:8080/api/todos';
+
+  constructor(
+    //Injeção de serviço, inicializa a clase
+    private http: HttpClient
+  ) {}
+  
+  //Observable: Espera o retorno da requisão.
+  save(todo: Todo) : Observable<Todo> {
+    //http.post<Object>
+    //Param n1: Url da api para a qual esta fazendo o post 
+    //Param n2: Objeto Json que deve ser enviado no body
+    return this.http.post<Todo>(this.apiUrl,todo)
+  }
+
+  //Retorna uma lista com todas as todos 
+  list () : Observable<Todo[]>{
+    return this.http.get<Todo[]>(this.apiUrl);    
+  }
+
+  //Deleta uma todo do bd
+  delete(id: number) : Observable<void>{
+    const url =  `${this.apiUrl}/${id}`
+    return this.http.delete<void>(url);
+  }
+
+  markAsDone(id: number): Observable<Todo>{
+    const url = `${this.apiUrl}/${id}/done`;
+    return this.http.patch<Todo>(url, {})
+  }
+}
